3a1b504539f99e3793ca1ca2da4dbf6bab01272f
unknown <hindhujhasridhar@gmail.com>
2021-12-27 15:22:42 +0530

    USING SWITCH CASE EMPLOYEE WAGE FOUND

---
+++ EmployeeWage/Switch.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    public class Switch:PartTime
+    {
+
+        public void switchcase()
+        {
+           
+
+            switch (empcheck)
+            {
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
+                 
+            }
+            empWage = empHrs * EMP_RATE_HR;
+            Console.WriteLine("EMPLOYEE WAGE IS:  " + empWage);
+          
+        }
+    }
+}
